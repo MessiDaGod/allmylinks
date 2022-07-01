bdebc851392939e691c8dffbafe5a744e5d40d3c
joeshakely <joeshakely@gmail.com>
2022-07-01 08:14:09 -0700

    add files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using allmylinks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>  
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
